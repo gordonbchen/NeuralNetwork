44232d870eda5533522c8e77229dff13a7f93be1
Gordon Chen <gordonbchen@gmail.com>
2023-05-25 09:03:26 -0400

    refactor methods

--- neural_network.py
+++ neural_network.py
@@ -6,18 +6,20 @@ class NeuralNetwork:
     def __init__(self):
         """Initialize weights and biases, and hyperparameters."""
         # Layer 1.
-        self.W1 = np.random.random(12 * 784).reshape(12, 784) - 0.5
-        self.b1 = np.random.random(12) - 0.5
+        l1_inputs, l1_nodes = 784, 12
+        self.W1 = np.random.random(l1_nodes * l1_inputs).reshape(l1_nodes, l1_inputs) - 0.5
+        self.b1 = np.random.random(l1_nodes) - 0.5
 
-        self.f1 = functions.ReLU
-        self.f1_deriv = functions.ReLU_deriv
+        self.f_act1 = functions.ReLU
+        self.f_act1_deriv = functions.ReLU_deriv
 
         # Layer 2.
-        self.W2 = np.random.random(10 * 12).reshape(10, 12) - 0.5
-        self.b2 = np.random.random(10) - 0.5
+        l2_inputs, l2_nodes = 12, 10
+        self.W2 = np.random.random(l2_nodes * l2_inputs).reshape(l2_nodes, l2_inputs) - 0.5
+        self.b2 = np.random.random(l2_nodes) - 0.5
 
-        self.f2 = functions.softmax
-        self.f2_deriv = functions.softmax_deriv
+        self.f_act2 = functions.softmax
+        self.f_act2_deriv = functions.softmax_deriv
 
         # Cost and accuracy functions.
         self.f_cost = functions.mean_squared_error
@@ -28,49 +30,6 @@ class NeuralNetwork:
         # Hyperparams.
         self.learning_rate = 0.1
 
-    def forward_prop(self, X):
-        """Forward-prop to get the network's output."""
-        X = X.T
-
-        Z1 = np.dot(self.W1, X) + self.b1[:, np.newaxis]
-        A1 = self.f1(Z1)
-
-        Z2 = np.dot(self.W2, A1) + self.b2[:, np.newaxis]
-        A2 = self.f2(Z2)
-        return Z1, A1, Z2, A2
-    
-    def back_prop(self, Z1, A1, Z2, A2, X, y):
-        """Back-prop to find the network's gradient."""
-        one_hot_Y = functions.one_hot_encode(y)
-
-        # TODO: cost function and softmax derivatives.
-        # dA2 = self.f_cost_deriv(A2, one_hot_Y)
-        # dZ2 = self.f2_deriv(dA2)
-
-        dZ2 = A2 - one_hot_Y
-
-        dW2 = np.dot(dZ2, A1.T) / y.size
-        db2 = np.mean(dZ2, axis=1)
-
-        dA1 = np.dot(self.W2.T, dZ2)
-        dZ1 = dA1 * self.f1_deriv(Z1)
-
-        dW1 = np.dot(dZ1, X) / y.size
-        db1 = np.mean(dZ1, axis=1)
-        return dW1, db1, dW2, db2
-    
-    def apply_gradients(self, dW1, db1, dW2, db2, learning_rate):
-        """Apply calculated gradients with a learning rate."""
-        self.W1 -= dW1 * learning_rate
-        self.b1 -= db1 * learning_rate
-        self.W2 -= dW2 * learning_rate
-        self.b2 -= db2 * learning_rate
-    
-    def gradient_descent(self, X, y):
-        """Gradient descent learning step."""
-        Z1, A1, Z2, A2 = self.forward_prop(X)
-        dW1, db1, dW2, db2 = self.back_prop(Z1, A1, Z2, A2, X, y)
-        self.apply_gradients(dW1, db1, dW2, db2, self.learning_rate)
 
     def train(self, X, y, epochs):
         """Train the network for the given epochs."""
@@ -93,3 +52,48 @@ class NeuralNetwork:
             np.random.shuffle(inds)
             X = X[inds, :]
             y = y[inds]
+
+
+    def gradient_descent(self, X, y):
+        """Gradient descent learning step."""
+        Z1, A1, Z2, A2 = self.forward_prop(X)
+        dW1, db1, dW2, db2 = self.back_prop(Z1, A1, Z2, A2, X, y)
+        self.apply_gradients(dW1, db1, dW2, db2, self.learning_rate)
+
+    def forward_prop(self, X):
+        """Forward-prop to get the network's output."""
+        X = X.T
+
+        Z1 = np.dot(self.W1, X) + self.b1[:, np.newaxis]
+        A1 = self.f_act1(Z1)
+
+        Z2 = np.dot(self.W2, A1) + self.b2[:, np.newaxis]
+        A2 = self.f_act2(Z2)
+        return Z1, A1, Z2, A2
+    
+    def back_prop(self, Z1, A1, Z2, A2, X, y):
+        """Back-prop to find the network's gradient."""
+        one_hot_Y = functions.one_hot_encode(y)
+
+        # TODO: cost function and softmax derivatives.
+        # dA2 = self.f_cost_deriv(A2, one_hot_Y)
+        # dZ2 = self.f2_deriv(dA2)
+
+        dZ2 = A2 - one_hot_Y
+
+        dW2 = np.dot(dZ2, A1.T) / y.size
+        db2 = np.mean(dZ2, axis=1)
+
+        dA1 = np.dot(self.W2.T, dZ2)
+        dZ1 = dA1 * self.f_act1_deriv(Z1)
+
+        dW1 = np.dot(dZ1, X) / y.size
+        db1 = np.mean(dZ1, axis=1)
+        return dW1, db1, dW2, db2
+    
+    def apply_gradients(self, dW1, db1, dW2, db2, learning_rate):
+        """Apply calculated gradients with a learning rate."""
+        self.W1 -= dW1 * learning_rate
+        self.b1 -= db1 * learning_rate
+        self.W2 -= dW2 * learning_rate
+        self.b2 -= db2 * learning_rate
