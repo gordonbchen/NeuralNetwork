fix cost and softmax derivatives

functions.py
@@ -8,23 +8,26 @@ def ReLU_deriv(Z):
 
 
 def softmax(Z):
-    Z -= np.max(Z, axis=0)
-    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+    safe_Z = Z - np.max(Z, axis=0)
+    e_Z = np.exp(safe_Z)
+    return e_Z / np.sum(e_Z, axis=0)
 
 def softmax_deriv(Z):
-    Z -= np.max(Z, axis=0)
+    safe_Z = Z - np.max(Z, axis=0)
 
-    e_Z = np.exp(Z)
-    col_sums = np.sum(e_Z, axis=0)[np.newaxis, :]
+    e_Z = np.exp(safe_Z)
+    col_sums = np.sum(e_Z, axis=0)
+
+    a = col_sums - e_Z
     
-    return ((col_sums - e_Z) * e_Z) / (col_sums ** 2)
+    return (a * e_Z) / (col_sums ** 2)
 
 
 def mean_squared_error(A, Y):
     return np.mean((A - Y) ** 2)
 
 def mean_squared_error_deriv(A, Y):
-    return (2 / Y.shape[1]) * (A - Y)
+    return (2 / Y.shape[0]) * (A - Y)
 
 
 def one_hot_encode(Y):

neural_network.py
@@ -37,7 +37,7 @@ class NeuralNetwork:
         self.training_accuracies = []
 
         # Hyperparams.
-        self.learning_rate = 0.1
+        self.learning_rate = 0.25
         self.mini_batch_size = 256
 
 
@@ -95,11 +95,8 @@ class NeuralNetwork:
         """Back-prop to find the network's gradient."""
         one_hot_Y = functions.one_hot_encode(y)
 
-        # TODO: cost function and softmax derivatives.
-        # dA2 = self.f_cost_deriv(A2, one_hot_Y)
-        # dZ2 = self.f2_deriv(dA2)
-
-        dZ2 = A2 - one_hot_Y
+        dA2 = self.f_cost_deriv(A2, one_hot_Y)
+        dZ2 = dA2 * self.f_act2_deriv(Z2)
 
         dW2 = np.dot(dZ2, A1.T) / y.size
         db2 = np.mean(dZ2, axis=1)